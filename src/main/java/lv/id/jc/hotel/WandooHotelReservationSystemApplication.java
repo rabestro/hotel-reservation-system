9e4367bcb1c8884e3e37bcef531f920fad425179
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-04-24 14:24:57 +0300

    Initial commit

---
+++ src/main/java/lv/id/jc/hotel/WandooHotelReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package lv.id.jc.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WandooHotelReservationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WandooHotelReservationSystemApplication.class, args);
+    }
+
+}
