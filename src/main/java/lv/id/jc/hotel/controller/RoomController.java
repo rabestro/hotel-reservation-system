de1423568a456ad8cc21156e8c8a24b428252e5e
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-04-26 10:02:03 +0300

    Add room-add.http

--- src/main/java/lv/id/jc/hotel/controller/RoomController.java
+++ src/main/java/lv/id/jc/hotel/controller/RoomController.java
@@ -1,11 +1,15 @@
 package lv.id.jc.hotel.controller;
 
+import lv.id.jc.hotel.dto.Credentials;
 import lv.id.jc.hotel.model.Room;
 import lv.id.jc.hotel.service.RoomService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -16,4 +20,10 @@ public record RoomController(RoomService roomService) {
     List<Room> rooms() {
         return roomService().findAll();
     }
+
+    @PostMapping("/add")
+    public void add(@RequestBody @Valid Room room) {
+        roomService().add(room);
+    }
+
 }
--- src/main/java/lv/id/jc/hotel/service/RoomServiceImpl.java
+++ src/main/java/lv/id/jc/hotel/service/RoomServiceImpl.java
@@ -11,7 +11,7 @@ public record RoomServiceImpl(RoomRepository roomRepository) implements RoomServ
 
     @Override
     public void add(Room room) {
-
+        roomRepository().save(room);
     }
 
     @Override
