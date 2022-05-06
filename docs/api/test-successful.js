f55520ba1759ebc567c77c26a166125bd0aefd4f
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-05-06 08:55:42 +0300

    Add tests for roomTypes.http

--- docs/api/test-successful.js
+++ docs/api/test-successful.js
@@ -2,5 +2,5 @@ client.test("Request executed successfully", () => {
     client.assert(response.status === 200, "Response status is not 200");
 
     const type = response.contentType.mimeType;
-    client.assert(type === "application/json", "Expected 'application/json' but received '" + type + "'");
+    client.assert(type === "application/hal+json", "Expected 'application/hal+json' but received '" + type + "'");
 });
