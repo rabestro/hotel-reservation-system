405236e8b8cc18e251953e867db333797cc2a238
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-05-13 21:23:42 +0300

    Add SpockConfig.groovy

---
+++ src/test/resources/SpockConfig.groovy
@@ -0,0 +1,6 @@
+spockReports {
+    set 'com.athaydes.spockframework.report.showCodeBlocks': true
+//    set 'com.athaydes.spockframework.report.outputDir': 'docs/spock-reports'
+    set 'com.athaydes.spockframework.report.projectName': 'Hotel Reservation System'
+    set 'com.athaydes.spockframework.report.projectVersion': 1.0
+}
