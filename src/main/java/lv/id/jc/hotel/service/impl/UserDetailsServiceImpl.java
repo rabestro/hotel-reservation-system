8d8dd4457a582acbd4e1c336effe81b0c552efbc
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-05-14 01:01:30 +0300

    Updated UserDetailsServiceImpl - record changed to class

--- src/main/java/lv/id/jc/hotel/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/lv/id/jc/hotel/service/impl/UserDetailsServiceImpl.java
@@ -1,17 +1,21 @@
 package lv.id.jc.hotel.service.impl;
 
 import lv.id.jc.hotel.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public record UserDetailsServiceImpl(UserRepository userRepository) implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository()
+        return userRepository
                 .findFirstByEmailIgnoreCase(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Not found: " + username));
     }
