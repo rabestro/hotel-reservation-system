e3ac258bb430d1e327141fa7f81692c4a0bafe6b
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-03-30 11:38:33 +0300

    style: Delete blank line

--- src/main/java/lv/id/jc/hotel/HotelReservationSystemApplication.java
+++ src/main/java/lv/id/jc/hotel/HotelReservationSystemApplication.java
@@ -9,5 +9,4 @@ public class HotelReservationSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(HotelReservationSystemApplication.class, args);
     }
-
 }
