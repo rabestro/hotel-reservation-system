a81962188deb42c846b4ad3e17c2269d3ca79aaa
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-04 14:44:22 +0300

    Refactor register method in RegistrationController
    Refactored the register method in RegistrationController for better readability and separation of concerns. Extracted the customer and user creation logic to separate private methods. This simplifies the understanding of the register method's primary function.

--- src/main/java/lv/id/jc/hotel/controller/RegistrationController.java
+++ src/main/java/lv/id/jc/hotel/controller/RegistrationController.java
@@ -1,5 +1,13 @@
 package lv.id.jc.hotel.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -9,39 +17,40 @@ import lv.id.jc.hotel.model.User;
 import lv.id.jc.hotel.model.dto.Credentials;
 import lv.id.jc.hotel.model.dto.Customer;
 import lv.id.jc.hotel.service.UserService;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 
 @Tag(
-        name = "Registration",
-        description = "Registration of clients in the hotel reservation system"
+      name = "Registration",
+      description = "Registration of clients in the hotel reservation system"
 )
 @RestController
 @RequiredArgsConstructor
 public class RegistrationController {
-    private final UserService userService;
+   private final UserService userService;
 
-    @Operation(
-            summary = "Register new user",
-            parameters = @Parameter(
-                    name = "credentials",
-                    required = true
-            )
-    )
-    @ApiResponse(
-            responseCode = "201",
-            description = "Created"
-    )
-    @PostMapping("/register")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Customer register(@RequestBody @Valid Credentials credentials) {
-        var user = userService.createUser(credentials, User.Role.CUSTOMER);
-        return new Customer(user.getName(), user.getEmail());
-    }
+   @Operation(
+         summary = "Register new user",
+         parameters = @Parameter(
+               name = "credentials",
+               required = true
+         )
+   )
+   @ApiResponse(
+         responseCode = "201",
+         description = "Created"
+   )
+   @PostMapping("/register")
+   @ResponseStatus(HttpStatus.CREATED)
+   public Customer register(@RequestBody @Valid Credentials credentials) {
+      var user = createUser(credentials);
+      return createCustomer(user);
+   }
 
+   private User createUser(Credentials credentials) {
+      return userService.createUser(credentials, User.Role.CUSTOMER);
+   }
+
+   private Customer createCustomer(User user) {
+      return new Customer(user.getName(), user.getEmail());
+   }
 }
