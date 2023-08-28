76faa660f663be5f2d0785053c0b90a34f728429
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-28 22:43:52 +0300

    Refactor User class and add object comparison methods
    The User.java class was refactored for the better reading experience. Import statements were formatted for easy reading and references. Locale.ENGLISH was added to the toLowerCase method to ensure uniformity across different Server setups. Equals and hashCode methods were overridden to enable object comparison based on email, which is a unique field. The serialVersionUID was also updated due to the structural change in the class. Also, "@Valid" is added to the Set<Reservation> to ensure the persistence of valid reservations only.

--- src/main/java/lv/id/jc/hotel/model/User.java
+++ src/main/java/lv/id/jc/hotel/model/User.java
@@ -1,13 +1,11 @@
 package lv.id.jc.hotel.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.data.jpa.domain.AbstractAuditable;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import java.io.Serial;
+import java.util.Collection;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -20,105 +18,104 @@ import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.io.Serial;
-import java.io.Serializable;
-import java.util.Collection;
-import java.util.List;
-import java.util.Locale;
-import java.util.Objects;
-import java.util.Set;
+
+import org.springframework.data.jpa.domain.AbstractAuditable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Getter;
+import lombok.Setter;
+
 
 @Getter
 @Setter
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 public class User extends AbstractAuditable<User, Long> implements UserDetails {
-    @Serial
-    private static final long serialVersionUID = 2L;
-    @NotBlank
-    @Column(nullable = false)
-    private String name;
-    @NotNull
-    @Column(nullable = false)
-    private User.Role role;
-    @Email
-    @Column(unique = true, nullable = false, length = 320, columnDefinition = "varchar_ignorecase(320)")
-    private String email;
-    @JsonIgnore
-    @Column(length = 60, columnDefinition = "char(60)")
-    private String password;
-    @Column(nullable = false, columnDefinition = "boolean default true")
-    private boolean enabled;
-    @OneToMany(mappedBy = "guest", fetch = FetchType.LAZY)
-    private Set<@Valid Reservation> reservations;
-    public User() {
-    }
-    public User(Role role, String name, String email, String password) {
-        this.name = name;
-        this.role = role;
-        this.email = email;
-        this.password = password;
-        this.enabled = true;
-    }
+   @Serial
+   private static final long serialVersionUID = 2L;
+   @NotBlank
+   @Column(nullable = false)
+   private String name;
+   @NotNull
+   @Column(nullable = false)
+   private User.Role role;
+   @Email
+   @Column(unique = true, nullable = false, length = 320, columnDefinition = "varchar_ignorecase(320)")
+   private String email;
+   @JsonIgnore
+   @Column(length = 60, columnDefinition = "char(60)")
+   private String password;
+   @Column(nullable = false, columnDefinition = "boolean default true")
+   private boolean enabled;
+   @OneToMany(mappedBy = "guest", fetch = FetchType.LAZY)
+   private Set<@Valid Reservation> reservations;
 
-    @PreUpdate
-    @PrePersist
-    private void prepare() {
-        email = email.toLowerCase(Locale.ENGLISH);
-    }
+   public User() {
+   }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(role.authority());
-    }
+   public User(Role role, String name, String email, String password) {
+      this.name = name;
+      this.role = role;
+      this.email = email;
+      this.password = password;
+      this.enabled = true;
+   }
 
-    @Override
-    public String getUsername() {
-        return getEmail();
-    }
+   @PreUpdate
+   @PrePersist
+   private void prepare() {
+      email = email.toLowerCase(Locale.ENGLISH);
+   }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
+   @Override
+   public Collection<? extends GrantedAuthority> getAuthorities() {
+      return List.of(role.authority());
+   }
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
+   @Override
+   public String getUsername() {
+      return getEmail();
+   }
 
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
+   @Override
+   public boolean isAccountNonExpired() {
+      return true;
+   }
 
-    @Override
-    public boolean isEnabled() {
-        return enabled;
-    }
+   @Override
+   public boolean isAccountNonLocked() {
+      return true;
+   }
 
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
+   @Override
+   public boolean isCredentialsNonExpired() {
+      return true;
+   }
 
-    public enum Role {
-        EMPLOYEE, CUSTOMER;
+   @Override
+   public boolean isEnabled() {
+      return enabled;
+   }
 
-        GrantedAuthority authority() {
-            return new SimpleGrantedAuthority("ROLE_" + name());
-        }
-    }
+   public void setEnabled(boolean enabled) {
+      this.enabled = enabled;
+   }
 
    @Override
    public boolean equals(final Object o) {
       if (this == o) {
-          return true;
+         return true;
       }
       if (o == null || getClass() != o.getClass()) {
-          return false;
+         return false;
       }
       if (!super.equals(o)) {
-          return false;
+         return false;
       }
       var user = (User) o;
       return Objects.equals(email, user.email);
@@ -127,5 +124,13 @@ public class User extends AbstractAuditable<User, Long> implements UserDetails {
    @Override
    public int hashCode() {
       return Objects.hash(super.hashCode(), email);
+   }
+
+   public enum Role {
+      EMPLOYEE, CUSTOMER;
+
+      GrantedAuthority authority() {
+         return new SimpleGrantedAuthority("ROLE_" + name());
+      }
    }
 }
